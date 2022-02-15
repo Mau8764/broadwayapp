84f24f9f09f23487f763ff58033b83ea5b0affb0
Mau8764 <Mau8764@github.com>
2022-02-14 19:44:19 -0600

    Review Controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :plays
+  resources :plays do 
+    resources :reviews
+  end
   
   root "plays#index"
 end
